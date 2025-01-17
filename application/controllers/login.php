b0f9c80ddb86a38b4b488992b953ee244e8ad9d2
Muhammad Ridwan Hakim, S.T., CPITA, ITPMCP <adminrescen@gmail.com>
2025-01-17 09:19:49 +0700

    Cleanup Code
    Signed-off-by: Muhammad Ridwan Hakim, S.T., CPITA, ITPMCP <adminrescen@gmail.com>

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,21 +1,22 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Login extends CI_Controller {
+class Login extends CI_Controller
+{
 
-	public function __construct() {
-    parent::__construct();
-    $this->load->helper('url');
-    $this->load->helper('download');
-	$this->load->library('pagination');
-	$this->load->helper('cookie');
-	$this->load->model('login_model');
-
-  }
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('url');
+		$this->load->helper('download');
+		$this->load->library('pagination');
+		$this->load->helper('cookie');
+		$this->load->model('login_model');
+	}
 
 	public function index()
 	{
-		
+
 		$this->load->view('templates/header_login');
 		$this->load->view('login/index');
 		$this->load->view('templates/footer_login');
@@ -24,38 +25,36 @@ class Login extends CI_Controller {
 	public function proses_login()
 	{
 		$username = $this->input->post('user');
-    	$password = $this->input->post('pwd');
+		$password = $this->input->post('pwd');
 
-    	$where = array(
-    		'nama' => $username,
-    		'pass' => $password
-    	);
+		$where = array(
+			'nama' => $username,
+			'pass' => $password
+		);
 
-    	$cek = $this->login_model->cek_login($where, 'pengguna')->num_rows();
-    	$data = $this->login_model->cek_login($where, 'pengguna')->row_array();
-    	if ($cek > 0) {
+		$cek = $this->login_model->cek_login($where, 'pengguna')->num_rows();
+		$data = $this->login_model->cek_login($where, 'pengguna')->row_array();
+		if ($cek > 0) {
 
-    		$sessi = array(
+			$sessi = array(
 				'id_user' => $data['id_user'],
-    			'username' => $data['nama'],
-    			'email' => $data['email'],
-    			'notelp' => $data['notelp'],
-    			'status' => $data['status'],
-    			'level' => $data['level'],
+				'username' => $data['nama'],
+				'email' => $data['email'],
+				'notelp' => $data['notelp'],
+				'status' => $data['status'],
+				'level' => $data['level'],
 				'foto' => $data['foto'],
 				'login' => 'perpusweb'
-    		);
+			);
 
 			$this->session->set_userdata($sessi);
-			
+
 			$respon = array('respon' => 'success');
 			echo json_encode($respon);
-    	}
-    	else{
+		} else {
 			$respon = array('respon' => 'failed');
 			echo json_encode($respon);
 		}
-
 	}
 
 	public function logout()
